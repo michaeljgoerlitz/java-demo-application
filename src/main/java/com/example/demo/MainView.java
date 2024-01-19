ddbd70c87328caa296f8b8ec8a330447c0978380
michaeljgoerlitz <mjgoerlitz@icloud.com>
2024-01-18 23:50:39 -0500

    added first button and text field components

--- src/main/java/com/example/demo/MainView.java
+++ src/main/java/com/example/demo/MainView.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 @Route("")
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        add(new H1("Hello world"));
+        var button = new Button("Click me");
+        var textField = new TextField();
+        add(new HorizontalLayout(textField, button));
     }
 }
