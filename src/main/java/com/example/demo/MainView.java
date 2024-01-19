62f45b9013c17743bfce52e78bd5fdf48d5ff707
michaeljgoerlitz <mjgoerlitz@icloud.com>
2024-01-19 00:11:26 -0500

    added more stuff to mainview file

--- src/main/java/com/example/demo/MainView.java
+++ src/main/java/com/example/demo/MainView.java
@@ -1,17 +1,25 @@
 package com.example.demo;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.Route;
 
 @Route("")
 public class MainView extends VerticalLayout {
 
     private PersonRepository repository;
+    private TextField firstName = new TextField("First Name");
+    private TextField lastName = new TextField("Last Name");
+    private EmailField email = new EmailField("Email");
+    private Binder<Person> binder = new Binder<>(Person.class);
+    private Grid<Person> grid = new Grid<>(Person.class);
 
     public MainView(PersonRepository repository) {
         this.repository = repository;
