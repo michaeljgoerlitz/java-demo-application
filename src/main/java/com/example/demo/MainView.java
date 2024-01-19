5f5ecd94aee5914ea46e199e5025b761732a2a03
michaeljgoerlitz <mjgoerlitz@icloud.com>
2024-01-18 23:52:19 -0500

    adding first event listener

--- src/main/java/com/example/demo/MainView.java
+++ src/main/java/com/example/demo/MainView.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -14,5 +15,10 @@ public class MainView extends VerticalLayout {
         var button = new Button("Click me");
         var textField = new TextField();
         add(new HorizontalLayout(textField, button));
+
+        button.addClickListener(e -> {
+           add(new Paragraph("Hello, " + textField.getValue()));
+           textField.clear();
+        });
     }
 }
